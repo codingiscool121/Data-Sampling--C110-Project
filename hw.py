1b3df3310d12cb19706ef7b003e92ba8f674e893
Coder 119 <65823748+codingiscool121@users.noreply.github.com>
2021-06-22 09:44:28 -0400

    Update hw.py

--- hw.py
+++ hw.py
@@ -5,15 +5,15 @@ import plotly.graph_objects as go
 import random as rand
 
 data = pd.read_csv("medium_data.csv")
-readtime = data["reading_time"]
+readtime = data["reading_time"].tolist()
 populationmean = st.mean(readtime)
 populationsd = st.stdev(readtime)
 print("The population mean is", populationmean)
 print("The population standard deviation is", populationsd)
-list=[]
 meanlist=[]
 
 def sampling():
+    list=[]
     for i in range(0,30):
         r=rand.randint(0,len(readtime)-1)
         list.append(readtime[r])
